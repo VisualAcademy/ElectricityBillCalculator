db48c5fdc03ee6d9557bcc63095102a83715d8a2
RedPlus <redplus@redplus.net>
2019-01-23 01:19:34 +0900

    단위 테스트 프로젝트 추가

---
+++ Dul/Dul.Tests/전기요금계산기테스트.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dul.Tests
+{
+    [TestClass]
+    public class 전기요금계산기테스트
+    {
+        /// <summary>
+        /// 전기요금계산기 클래스 테스트 
+        /// </summary>
+        [TestMethod]
+        public void 클래스기본테스트()
+        {
+            var cls = new 전기요금계산기();
+            Assert.AreEqual("Dul." + nameof(전기요금계산기), cls.ToString());
+        }
+    }
+}
